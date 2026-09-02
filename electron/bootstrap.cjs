8d4aa12330c41218d56ca51e1111264e19f792c3
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-02 20:04:58 +0800

    feat: load agent live follow watcher

--- electron/bootstrap.cjs
+++ electron/bootstrap.cjs
@@ -5,5 +5,6 @@ process.env.OPENAI_AGENT_MODEL =
 require('./file_manager_runtime_patch.cjs');
 require('./file_manager_ipc.cjs');
 require('./terminal_ipc.cjs');
+require('./agent_follow_ipc.cjs');
 require('./agent_ipc.cjs');
 require('./main.cjs');
