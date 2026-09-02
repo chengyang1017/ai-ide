f1cc9d0dcf7473e5a57a9c50a689294dc8dd5839
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-02 19:11:35 +0800

    feat: load explorer multi-select controller

--- src/project/file_manager_bootstrap.ts
+++ src/project/file_manager_bootstrap.ts
@@ -12,8 +12,9 @@ if (
   typeof bridge?.createProjectFile
     === 'function'
 ) {
-  void Promise.all([
-    import('./file_manager'),
-    import('./file_manager_create_dialog'),
-  ]);
+  void (async () => {
+    await import('./file_manager');
+    await import('./file_manager_create_dialog');
+    await import('./file_manager_multiselect');
+  })();
 }
